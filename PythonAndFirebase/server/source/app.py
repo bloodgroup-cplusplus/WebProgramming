2433021bde2489944dd49865eed9d5ce18c977e0
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2022-08-29 21:58:11 +0530

    added requirements.txt

---
+++ PythonAndFirebase/server/source/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+import os 
+
+app = Flask (__name__)
+
+
+
+@app.route("/")
+
+
+def index():
+    return render_tempalte("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host = '0.0.0.0', port = int(os.environ.get("PORT", 8080)))
+
