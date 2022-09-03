b9c67c97fd97aa2eecf5463f549b2932586a15ec
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2022-09-03 12:59:58 +0530

    added options. js

--- ChromeExtensionDevelopment/Timer/options.js
+++ ChromeExtensionDevelopment/Timer/options.js
@@ -1,3 +1,7 @@
-console.log("Hello from the options page!")
+const nameInput= document.getElementById("name-input")
+const saveBtn = document.getElementById("save-btn")
 
+saveBtn.addEventListener("click", () =>{
+    console.log(nameInput.value)
+})
 
