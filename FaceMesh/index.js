b09984805e454605253f10c07eb527a70273f4f9
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2022-09-08 13:47:00 +0530

    this is messud up

--- FaceMesh/index.js
+++ FaceMesh/index.js
@@ -1,13 +1,33 @@
-const faceLandmarksDetection = require('@tensorflow-models/face-landmarks-detection');
+import faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+const video = document.getElementById("video");
+
+const setupCamera =()=>{
+    navigator.mediaDevices.getUserMedia({
+        video:{width:600,height:400},
+        audio:false,
+    })
+    .then((stream)=>{
+        video.srcObject=stream;
+    });
+};
+const detectFaces = async()=>{
+    const prediction = model.estimateFaces({input:video},false);
+    console.log(prediction)
+};
 
 
 // load the faceLandmarksDetection model assets.
+setupCamera();
 
-const model = await faceLandmarksDetection.SupportedPackages.mediapipeFacemesh;
+video.addEventListener("lodedata", async()=>{
+    model= await faceLandmarksDetection.load(faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh);
+    setInterval(detectFaces,40);
 
-const video = document.getElementById("video");
+});
 
 
-const faces= await model.estimateFaces( {input:video});
+
+
+
 
 //https://blog.tensorflow.org/2020/11/iris-landmark-tracking-in-browser-with-MediaPipe-and-TensorFlowJS.html
