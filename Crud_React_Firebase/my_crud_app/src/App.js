2f10e24d2ab94df98b3604d17ba11afba53a57ee
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-12 11:08:45 +0530

    added crud operation

--- Crud_React_Firebase/my_crud_app/src/App.js
+++ Crud_React_Firebase/my_crud_app/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
 import {useState,useEffect} from "react";
+import './App.css';
 import {db} from './config'
-import{collection,getDocs,addDoc} from 'firebase/firestore';
+import{collection,getDocs,addDoc,updateDoc,doc, deleteDoc} from 'firebase/firestore';
 
 function App() {
   const[newName,setNewName]=useState("");
   const[newAge,setNewAge]=useState(0);
   const [users,setUsers] =useState([]);
   const usersCollectionRef=collection(db,"users");
+
+  //C= create a new user
   const createUser=async()=>{
 
     // this takes two things 
@@ -15,10 +17,27 @@ function App() {
     // it also takes in object containng the payload 
     // the data we want to add is the object containing name
 
-    await addDoc(usersCollectionRef,{name:newName,age:newAge});
+    await addDoc(usersCollectionRef,{name:newName,age:Number(newAge)});
 
 
   };
+
+
+  const updateUser= async (id,age)=>{
+
+    const userDoc=doc(db,"users",id)
+    const newFields={age:age+1}
+    await updateDoc(userDoc,newFields);
+  }
+
+
+
+  const deleteUser=async(id)=>{
+    const userDoc=doc(db,"users",id);
+    await deleteDoc(userDoc);
+  }
+
+
   useEffect(()=>{
 
     const getUsers=async () =>{
@@ -26,14 +45,16 @@ function App() {
       setUsers(data.docs.map((doc)=>({...doc.data(),id:doc.id})));
 
     };
+    //R= Read a user 
     getUsers();
 
   },[])
 
 
-  return <div className="App">
+  return (<div className="App">
     
-    <input placeholder="Name...." 
+    <input 
+    placeholder="Name...." 
     onChage={(event)=>{
       setNewName(event.target.value);
     }}
@@ -47,12 +68,30 @@ function App() {
      />
     <button onClick={createUser}>Create User</button>
     {users.map((user)=>{
-    return <div><h1>Name:{user.name}</h1>
-    <h1>Age:{user.age}</h1>
+    return (
+    <div>
+      {" "}
+      <h1>Name:{user.name}</h1>
+      <h1>Age:{user.age}</h1>
+      <button 
+        onClick={() =>{updateUser(user.id,user.age)}}
+      >
+        {" "}
+        Increase Age: </button>
+        <button 
+          onClick={() =>{
+            deleteUser(user.id);
+          }}
+          >
+            {" "}
+            Delete User
+          </button>
     </div>
+    );
   })}
     </div>
   
+  );
 }
 
 export default App;
