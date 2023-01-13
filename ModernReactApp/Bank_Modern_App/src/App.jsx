553042c35cc0655c2bb51ce37ae57f80a2fc835e
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-13 19:20:24 +0530

    added how components and styles are structured in a react app

--- ModernReactApp/Bank_Modern_App/src/App.jsx
+++ ModernReactApp/Bank_Modern_App/src/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import styles from "./style";
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <h1>Hello,World!</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats 
+
+          Business 
+
+          CarDeal 
+
+          Testimonials 
+
+          Clients 
+
+          CTA 
+
+          Footer
+        </div>
+      </div>
 
     </div>
   )
