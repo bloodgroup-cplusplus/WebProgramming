9bbca591b79981a026fe8b41d7fb68973b8e6dd5
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-14 10:31:38 +0530

    added browser router code all the routes we are using is browser router the memory router (used for testing) hash router (used in shared servers) Static Routers ( used when server has only a single page to render ) can be skipped for now

--- ReactRouter/react_routers/src/App.js
+++ ReactRouter/react_routers/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {Link,Route,Routes} from "react-router-dom";
+import {Home} from './pages/Home';
+import {BookList} from './pages/BookList';
+function App()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+{
+  return(
+    <>
+
+      <nav>
+
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/books">Books</Link></li>
+        </ul>
+
+
+      </nav>
+
+     <Routes>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/books" element={<BookList/>}/>
+     </Routes>
+  </>
+  )
 }
 
 export default App;
