a51a3b1977ab463c41b23b9c360ee409ffa4dff0
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-19 14:18:49 +0530

    added compnents and props

--- ReactProjectBasedLearning/Project2/project2/src/App.js
+++ ReactProjectBasedLearning/Project2/project2/src/App.js
@@ -1,4 +1,15 @@
 import './App.css';
+// this is an example of react components 
+const Person=(props)=>{
+  return (
+    <>
+    <h1>Name :{props.name}</h1>
+    <h2> last Name:{props.lastName}</h2>
+    <h2> Age:30</h2>
+    </>
+  )
+}
+// Props are a way of adding arguments , properties to react components 
 
 const App =() => {
   //const name= 'Chad'
@@ -7,13 +18,18 @@ const App =() => {
   const isNameShowing = false;
   return (
     <div className="App">
+      <Person name={'John'} lastName={'Bosco'}/>
       <h1> Hello, {isNameShowing?name :"someone"}</h1>
       {name? (
           <>
           test
           </>
          ) : (
+
+          <>
           <h1> test</h1>
+          <h2> THere is no name</h2>
+          </>
          )}
 
       
