6d28f2bc45292c861fd97c1f857ecaed7a5f068f
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-19 18:20:14 +0530

    integrating ombd movie api

--- ReactProjectBasedLearning/Project2/actual_project/src/App.js
+++ ReactProjectBasedLearning/Project2/actual_project/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {useEffect} from 'react';
+
+const API_URL="http://www.omdbapi.com/?i=tt3896198&apikey=fbd00eaa";
 
 const App = () =>{
+  const searchMovies=async (title)=>{
+    const response= await fetch(`${API_URL} &s=${title}`);
+    const data= await response.json()
+    console.log(data);
+
+
+
+  }
+
+  useEffect(()=>{
+    searchMovies("Spiderman")
+  },[])
 
   return (
+    <h1>Return </h1>
 
-    <h1>App</h1>
   )
 
 
