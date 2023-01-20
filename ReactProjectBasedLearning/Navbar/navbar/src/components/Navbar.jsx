8cdc4cc98e272738c43cc85a1869545fcce20470
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-20 21:16:30 +0530

    use state not working

--- ReactProjectBasedLearning/Navbar/navbar/src/components/Navbar.jsx
+++ ReactProjectBasedLearning/Navbar/navbar/src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import {Component} from "react";
 import {MenuData} from "./MenuData";
+import {useState} from "react"
 import "./NavbarStyles.css";
 
 class Navbar extends Component 
 {
+    const [clicked,handleClick]= useState(0);
+    
     render(){
         return (
             <nav className="NavbarItems">
-                <h1>Sikkim Teachers Association </h1>
-                <ul>
+                <h1 classname="logo">STA<i className="fab fa-react"></i> </h1>
+                <div className="menu-icons" onclick={()=>handleClick(clicked=1)}>
+                    <i className={this.state.clicked?"fas-fa-times":"fas fa-bars"}></i>
+                </div>
+                <ul className="nav-menu">
                     {MenuData.map((item,index)=>{
                         return (
                         <li key={index}><a href={item.url} className={item.cName}>
