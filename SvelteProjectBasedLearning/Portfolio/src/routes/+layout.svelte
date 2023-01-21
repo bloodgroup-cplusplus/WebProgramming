7f8776f255e46746538a12df212fdcf104b2da02
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-21 21:58:05 +0530

    content is in prismic will do it later

---
+++ SvelteProjectBasedLearning/Portfolio/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {pico} from "@picocss/pico"
+
+
+</script>
+
+
+<nav class="layout">
+    <a prefetch href="/">MY SITE</a>
+</nav>
+
+<main>
+
+</main>
+
+
+<slot/>
+
+<footer> @Chad Coorporation 2022</footer>
