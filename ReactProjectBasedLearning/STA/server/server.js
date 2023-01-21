b16a706194e71bcf4029d2045bf1cae5cc64b078
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-21 13:07:02 +0530

    added server code

---
+++ ReactProjectBasedLearning/STA/server/server.js
@@ -0,0 +1,59 @@
+const app=require("express")()
+const Razorpay= require('razorpay')
+const cors= require('cors')
+const bodyParser=require('body-parser')
+const crypto= require('crypto')
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+var razorpay= new Razorpay({
+    key_id:"rzp_test_O1TBRbokgN31gn",
+    key_secret:"hj6ZSnz00dSxNCmYA3ZOcOZM"
+})
+
+
+app.post('/verification',(req,res)=>{
+    const secret="123456789"
+    console.log(req.body)
+    const shasum=crypto.createHmac('sha256',secret)
+    shasum.update(JSON.stringify(req.body))
+    const digest=shasum.digest('hex')
+    if(digest==req.headers['x-razorpay-signature'])
+    {
+        console.log("request is legit")
+    }
+    else
+    {
+        console.log("request is not legit")
+    }
+    res.json({status:'ok'})
+})
+
+
+app.post('/razorpay',async(req,res)=>{
+    const payment_capture=1
+    const amount="40000";
+    const currency="INR"
+    const options={
+        amount:amount,
+        currency,
+        payment_capture
+    }
+    try{
+        const response=await razorpay.orders.create(options)
+        console.log(response)
+    }catch(error)
+    {
+        console.log(error)
+    }
+})
+
+app.listen(1337,()=>{
+    console.log("listening at port 1337")
+})
+
+
+
