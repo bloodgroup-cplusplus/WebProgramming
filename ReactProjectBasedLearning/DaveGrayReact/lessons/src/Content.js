7f8634b245d0eb7500c407c14a6159f29f9d51e7
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-23 18:33:15 +0530

    added state varaibels

--- ReactProjectBasedLearning/DaveGrayReact/lessons/src/Content.js
+++ ReactProjectBasedLearning/DaveGrayReact/lessons/src/Content.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import {useState} from 'react';
+
 
 const Content = () => {
+    const[name,setName]=useState("")
+    const[count,setCount]=useState(0);
     const handleNameChange=()=>{
-        const names=["Bob","Kevin","Dave"];
+        const names=["Berus","Kyle","Chad"];
         const int = Math.floor(Math.random()*3);
-        return names[int];
+        setName(names[int]);
     }
 
     const handleClick=()=>{
-        console.log("You clicked it")
+        console.log(count)
+        setCount(count+1)
+        setCount(count+1)
+        // the above two lines doesn't work like traditional system ( doesn't increase ount by 2
+        // instead it increases value by 1 only)
     }
 
     const handleClick2=(name) =>{
@@ -21,9 +29,13 @@ const Content = () => {
   return (
     <main>
         <p>
-            Hello {handleNameChange()}!
+             Hello {name}!
+            <br></br>
+             the count is {count}!
         </p>
-        <button onClick={handleClick}>Click it</button>
+        <button onClick={handleNameChange}>Chage Name</button>
+        <button onClick={handleClick}>Countr</button>
+        
         <button onClick={()=> {handleClick2('Dave')}}>Click It</button>
         <button onClick={(e)=>handleClick3(e)}>Click It</button>
     </main>
