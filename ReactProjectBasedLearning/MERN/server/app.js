ea9eca2886015fba9be573e726488153590552b1
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-01-30 08:32:01 +0530

    added server code for displaying hello world on a port

---
+++ ReactProjectBasedLearning/MERN/server/app.js
@@ -0,0 +1,9 @@
+const express= require('express');
+const app=express();
+
+app.get('/',(req,res)=> res.send("hello world"))
+
+const port=process.env.PORT || 6969;
+
+
+app.listen(port, () => console.log("Server is running "))
