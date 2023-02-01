419e132ad112be0e594c891a52a5f47cd9f22d03
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-01 08:31:42 +0530

    final housekeeping , comments, modifications done

--- ReactProjectBasedLearning/UdemyCourse/netninja/src/components/NewEventForm.js
+++ ReactProjectBasedLearning/UdemyCourse/netninja/src/components/NewEventForm.js
@@ -1,27 +1,64 @@
 import './NewEventForm.css'
-import { useState } from 'react'
+import { useState,/*useRef*/ } from 'react'
 
 export default function NewEventForm({addEvent})
 {
     const[title,setTitle]=useState('')
     const[date,setDate]=useState('')
+    const[location,setLocation]=useState('Liverpool')
+
+
+
+    /* this is how to do the form thing using useRef hooks this is the declaration part
+
+
+   const title=useRef()
+   const date=useRef()*/
+
+
     const resetForm=()=>{
+
+
         setTitle('')
         setDate('')
+        setLocation('Liverpool')
+
+
+     /* updating using ref instead of using state variables 
+        title.current.value=''
+        date.current.value=''
+
+        */
     }
 
     const handleSubmit=(e)=>{
 
         // the default behaviour is to refresh the page when form is submitted 
         // we want to handle and do something else 
+
         e.preventDefault()
-        // collect all of the data we need for new event 
-        const event={
+        console.log(title,date)
+
+        // collect all of the data we need for new event this is how we collect using useRef hook 
+
+
+/*        const event={
+            title:title.current.value,
+            date:date.current.value,
+            id:Math.floor(Math.random()*100000)
+        }*/
+
+        // this is how we collect form data using state 
+
+        const event ={
             title:title,
             date:date,
+            location:location,
             id:Math.floor(Math.random()*100000)
         }
-        console.log(event)
+
+
+        //console.log(event)
         // everytime user submits the form reset it 
         addEvent(event)
         resetForm()
@@ -35,8 +72,9 @@ export default function NewEventForm({addEvent})
             
                 <input 
                 type="text" 
-                onChange={(e)=>setTitle(e.target.value)} 
                 id="title"
+                onChange={(e)=>setTitle(e.target.value)}
+                //ref={title} ref is used when we collect data using ref hook we use value for state
                 value={title}
                 />
             </label>
@@ -45,8 +83,18 @@ export default function NewEventForm({addEvent})
                 <input 
                 type= "date"
                 onChange={(e)=>setDate(e.target.value)}
+                //ref={date}
                 value={date}
                  />
+            </label>
+            <label>
+                <span> Event location :
+                    <select onChange={(e)=>setLocation(e.target.value)}>
+                        <option value="Liverpool">Liverpool</option>
+                        <option value="London">London</option>
+                        <option value="Cardiff">Cardiff</option>
+                    </select>
+                </span>
             </label>
 
             <button>Submit</button>
