e3164c98509ac95d2e51fa6e4f8e2a86d993d0c7
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-02 18:11:38 +0530

    added login component

--- ReactProjectBasedLearning/UdemyCourse/reactandfirebase/src/Pages/login/Login.js
+++ ReactProjectBasedLearning/UdemyCourse/reactandfirebase/src/Pages/login/Login.js
@@ -1,11 +1,48 @@
+
+// react hooks 
+import {useState} from 'react'
+
+// styles 
+
 import styles from './Login.module.css'
+
 
 import React from 'react'
 
 export default function Login() {
+  const [sta, setSta] = useState('')
+  const[name, setName] = useState('')
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(sta,name)
+  }
   return (
-    <div>
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>Login</h2>
+      <lablel >
+        <span>
+          STA ID:
+        </span>
+        <input
+          type="text"
+          onChange={(e)=>setSta(e.target.value)}
+          value={sta}
+          />
+      </lablel>
+
+      <label>
+        <span>Name:</span>
+        <input 
+        type="text"
+        onChange={(e)=>setName(e.target.value)}
+        value={name}/>
+
+      </label>
+
+      <button className='btn'>Login</button>
+
+
+    </form>
       
-    </div>
   )
 }
