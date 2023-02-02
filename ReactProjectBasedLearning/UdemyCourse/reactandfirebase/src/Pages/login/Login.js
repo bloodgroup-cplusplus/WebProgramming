800c1b2d88cb02ac743ae90c8b06c0bb3a389706
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-02 18:02:07 +0530

    added css for login form

--- ReactProjectBasedLearning/UdemyCourse/reactandfirebase/src/Pages/login/Login.js
+++ ReactProjectBasedLearning/UdemyCourse/reactandfirebase/src/Pages/login/Login.js
@@ -1,11 +1,10 @@
-//import styles from './Login.module.css'
+import styles from './Login.module.css'
 
 import React from 'react'
 
 export default function Login() {
   return (
     <div>
-      This is login 
       
     </div>
   )
