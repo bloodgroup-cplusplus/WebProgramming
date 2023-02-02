7a1e1fa5dfa98232740ed6ecefaa624e4a20a544
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-02 13:57:08 +0530

    added router 6 standards

---
+++ ReactProjectBasedLearning/UdemyCourse/reactroutersix/src/pages/About.js
@@ -0,0 +1,25 @@
+import {Route} from "react-router-dom"
+
+// nested routes 
+
+
+import Offers from "./Offers"
+
+
+export default function About()
+{
+    return (
+        <div className="content">
+            <h2>About us</h2>
+            <p>sdfadsfadsfasdflsdkjfa;lsdkf adskf ds</p>
+
+
+            <p> asdf;akldsfjasdfoadjsfd adslfj dsfkl; jadsf;adksf</p>
+    
+    <Route path = "/about/offers">
+        <Offers/>
+        </Route>
+    </div>
+    )
+
+}
---
+++ ReactProjectBasedLearning/UdemyCourse/reactroutersix/src/pages/Home.js
@@ -0,0 +1,13 @@
+export default function Home()
+{
+    return (
+        <div className="content">
+            <h1>Welcome ...</h1>
+            <p>Quality clothing for Quality Ninja</p>
+            <img src="https://via.placeholder.com/1200x200" alt="site banner"/>
+            <p> 3 and half hour is long than a JRE pod. I'm there still going there 
+                He's a military guy that is trying him to drill . A fake blue person 
+            </p>
+        </div>
+    )
+}
