8da09c0d57f7e6845742e5161f2b537a8ff3fa97
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-02 09:46:27 +0530

    creating basic svelte component adding js adding styles adding basic components etc

--- Svelte/UdemyCourse/svelte-course/src/App.svelte
+++ Svelte/UdemyCourse/svelte-course/src/App.svelte
@@ -1,45 +1,25 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  let name = "chad";
+  let src="http://picsum.com/200/200";
+  let string="Some <strong> string </string> with html "
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+<h1>
+  Hello {name.toUpperCase()}!
+</h1>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+<img src={src} alt={`Some text ${name}`}/>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<p>{@html string}</p>
+
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  /* we can directly use h1 instead of classname in styles cause svelte will directly generate
+  a hash of html components so that we can directly use it in our styles this is called stlye copying 
+  in svelte  */
+
+  h1{
+    color: red;
   }
 </style>
--- Svelte/UdemyCourse/svelte-course/src/main.js
+++ Svelte/UdemyCourse/svelte-course/src/main.js
@@ -1,8 +1,12 @@
+
+import App from './App.svelte';
 import './app.css'
-import App from './App.svelte'
 
-const app = new App({
-  target: document.getElementById('app'),
-})
+const app = new App(
+  {
+    target: document.getElementById('app')
+  }
+)
 
-export default app
+
+export default App;
