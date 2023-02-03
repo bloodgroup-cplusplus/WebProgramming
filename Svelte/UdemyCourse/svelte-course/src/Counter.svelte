3fae67e79cccdc22a3a641d6061067fd3eb6ad4d
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-03 09:00:13 +0530

    we need to use reactive statements in order to update how our reactive state updates

--- Svelte/UdemyCourse/svelte-course/src/Counter.svelte
+++ Svelte/UdemyCourse/svelte-course/src/Counter.svelte
@@ -1,12 +1,21 @@
 <script>
     // to create state in svelte we simply define new variable
+    // unlike react state which will be re-rendered each time the
+    // value of the state variable changes . The state components in svelte
+    // only run once so we see that the value of the count increases during
+    // function call ( due to function call )
+    // but the string here which is inside the
     let count = 0;
+    // let's say our string depends on count state
+    let string = `You have clicked ${count} times`;
+
     function increment() {
         count += 1;
     }
 </script>
 
 <button on:click={increment}> Clicks{count}</button>
+<h3>{string}</h3>
 
 <style>
     button {
