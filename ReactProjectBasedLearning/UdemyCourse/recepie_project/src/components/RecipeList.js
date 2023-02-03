34659b10c1628ce8f0b47225f7b04f1891eebd63
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-03 20:12:01 +0530

    added css to the component

---
+++ ReactProjectBasedLearning/UdemyCourse/recepie_project/src/components/RecipeList.js
@@ -0,0 +1,27 @@
+
+// styles
+
+import "./RecipeList.css"
+
+
+import React from 'react'
+
+
+import {Link} from "react-router-dom"
+
+export default function RecipeList({recipes}) {
+  return (
+    <div className="recipe-list">
+        { recipes.map(recipe=>(
+            <div key={recipe.id} className="card">
+                <h3> {recipe.title}</h3>
+                <p>{recipe.cookingTime} to make.</p>
+                <div>{recipe.method}...</div>
+                <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+
+                </div>
+        ))} 
+    </div>
+  )
+}
+
--- ReactProjectBasedLearning/UdemyCourse/recepie_project/src/pages/home/Home.js
+++ ReactProjectBasedLearning/UdemyCourse/recepie_project/src/pages/home/Home.js
@@ -3,15 +3,17 @@ import {useFetch} from '../../hooks/useFetch'
 
 
 import './Home.css'
+
+
+// import components 
+import RecipeList from '../../components/RecipeList'
 export default function Home() {
     const {data,isPending,error} = useFetch("http://localhost:3000/recipes")
   return (
     <div className='home'>
         {error && <p className='error'>{error}</p>}
         {isPending && <p className='loading'>Loading ...</p>}
-        {data && data.map(recipe=>(
-            <h2 key={recipe.id}>{recipe.title}</h2>
-        ))}
+        {data && <RecipeList recipes={data}/>}
       
     </div>
   )
