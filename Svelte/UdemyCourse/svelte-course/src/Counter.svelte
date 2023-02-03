fdc821d59f38627197ea0d8df38b54713d6c850d
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-03 09:11:39 +0530

    how to create reactive statements in svelte

--- Svelte/UdemyCourse/svelte-course/src/Counter.svelte
+++ Svelte/UdemyCourse/svelte-course/src/Counter.svelte
@@ -7,7 +7,30 @@
     // but the string here which is inside the
     let count = 0;
     // let's say our string depends on count state
-    let string = `You have clicked ${count} times`;
+    //let string = `You have clicked ${count} times`;
+    // this string value does not update unless we use reactive statement
+    // we have something like this i.e., a varialbe that depends on a state
+    // and this variable to update when state updates
+    // we can use reactive statements the format is
+    /* $: {
+        console.log(`The count is ${count}`)
+    }*/
+    // this above statement runs every time when count changes
+    // let's write this within reactive statements
+    // any dependency on the reactive varialbe changes the statement inside
+    // the $sign runs again and again
+    // define your string in a reactive statement
+    let string;
+    $: {
+        string = `You have clicked ${count} times.`;
+    }
+    // you could also write your reactive variables like this
+
+    $: string_two = `You have clicked ${count} times.`;
+
+    $: if (count > 5) {
+        console.log(`The count is ${count}`);
+    }
 
     function increment() {
         count += 1;
@@ -16,6 +39,7 @@
 
 <button on:click={increment}> Clicks{count}</button>
 <h3>{string}</h3>
+<h4>{string_two}</h4>
 
 <style>
     button {
