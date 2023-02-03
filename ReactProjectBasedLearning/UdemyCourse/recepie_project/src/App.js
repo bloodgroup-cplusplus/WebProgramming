4194b648f9a97019f78eb7e5f225e4c2879d4908
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-03 18:48:43 +0530

    added css to the project

--- ReactProjectBasedLearning/UdemyCourse/recepie_project/src/App.js
+++ ReactProjectBasedLearning/UdemyCourse/recepie_project/src/App.js
@@ -4,18 +4,23 @@ import './App.css';
 
 import{BrowserRouter, Routes, Route} from 'react-router-dom'
 
-// page components 
+// import pages  
 
 import Home from './pages/home/Home'
 import Create from './pages/create/Create'
 import Search from './pages/search/Search'
 import Recipe from './pages/recipe/Recipe'
 
+// import components 
+
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <div className="App">
       Happy hour
       <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path = "/" element={<Home/>}/>
         <Route path ="/create" element={<Create/>}/>
---
+++ ReactProjectBasedLearning/UdemyCourse/recepie_project/src/components/Navbar.js
@@ -0,0 +1,25 @@
+// import styles 
+
+import './Navbar.css'
+
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className='navbar' >
+    <nav>
+        <Link to="/">
+            <h1>Cooking Ninja</h1>
+        </Link>
+
+        <Link to="/create">Create Recipe</Link>
+
+    </nav>
+
+    </div>
+    
+  )
+}
+
+export default Navbar
