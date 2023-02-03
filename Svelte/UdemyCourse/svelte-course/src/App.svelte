0d10022c0b8c53c0130c20143608ef220a91ea88
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-03 08:43:25 +0530

    creating a state variable in svelte

--- Svelte/UdemyCourse/svelte-course/src/App.svelte
+++ Svelte/UdemyCourse/svelte-course/src/App.svelte
@@ -1,25 +1,30 @@
 <script>
-  let name = "chad";
-  let src="http://picsum.com/200/200";
-  let string="Some <strong> string </string> with html "
+    //let name = 'chad';
+    //let src = 'http://picsum.com/200/200';
+    //let string = 'Some <strong> string </string> with html ';
+
+    // import components
+
+    import Counter from './Counter.svelte';
 </script>
 
-
-<h1>
-  Hello {name.toUpperCase()}!
+<!-- this was some introductory html-->
+<!--h1>
+    Hello {name.toUpperCase()}!
 </h1>
+<img {src} alt={`Some text ${name}`} />
+<p>{@html string}</p!-->
 
-<img src={src} alt={`Some text ${name}`}/>
+<!--We can simply use the component this way-->
 
-<p>{@html string}</p>
-
+<Counter />
 
 <style>
-  /* we can directly use h1 instead of classname in styles cause svelte will directly generate
+    /* we can directly use h1 instead of classname in styles cause svelte will directly generate
   a hash of html components so that we can directly use it in our styles this is called stlye copying 
   in svelte  */
 
-  h1{
+    /* h1{
     color: red;
-  }
+  }*/
 </style>
---
+++ Svelte/UdemyCourse/svelte-course/src/Counter.svelte
@@ -0,0 +1,15 @@
+<script>
+    // to create state in svelte we simply define new variable
+    let count = 0;
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<button on:click={increment}> Clicks{count}</button>
+
+<style>
+    button {
+        padding: 15px;
+    }
+</style>
