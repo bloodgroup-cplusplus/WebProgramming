278266702ac85545f9b5a9f6dbfab5a0e0d5e403
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-04 16:42:27 +0530

    added controllers

--- NodejsFCC/openaiclone/index.js
+++ NodejsFCC/openaiclone/index.js
@@ -8,6 +8,13 @@ const port = process.env.PORT || 5000
 const app = express();
 
 
+//Enable body parser
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
 app.use("/openai", require('./routes/openaiRoutes'));
 
 
--- NodejsFCC/openaiclone/routes/openaiRoutes.js
+++ NodejsFCC/openaiclone/routes/openaiRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express')
+const{generateImage} = require('../controllers/openaiController')
+
+
 
 const router = express.Router()
 
 
-router.post('/generateimage',(req,res)=>{
-    res.status(200).json({
-        success:true
-    })
-})
-
+router.post('/generateimage',generateImage);
 module.exports = router
