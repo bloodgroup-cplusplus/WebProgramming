65a7f8a5949b36e12a68da69bf77ca5bb4ad4e03
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-04 14:42:20 +0530

    craeted form to create new recipes

--- ReactProjectBasedLearning/UdemyCourse/recepie_project/src/pages/create/Create.js
+++ ReactProjectBasedLearning/UdemyCourse/recepie_project/src/pages/create/Create.js
@@ -1,12 +1,61 @@
 // styles 
+// import use state hook 
+
 
 import './Create.css'
+import {useState} from 'react'
 
 
 export default function Create() {
+  const[title,setTitle]= useState('')
+  const[method,setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+  const handleSubmit = (e) =>{
+
+    e.preventDefault()
+    console.log(title,method,cookingTime)
+
+  }
+
+
   return (
-    <div>
-        Create
+    <div className='create'>
+      <h2 className='page-title'>Add a new Recipe</h2>
+      <form onSubmit={handleSubmit} >
+        <label>
+          <span>Recipe title:</span>
+          <input 
+            type="text"
+            onChange={(e)=>setTitle(e.target.value)}
+            value={title}
+            required
+            />
+        </label>
+        {/*ingridents go here */}
+
+        <label>
+          <span> Recipe Method:</span>
+          <textarea
+            onChange={(e)=>setMethod(e.target.value)} 
+            value={method}
+            required
+            />
+        </label>
+
+        <label>
+          <span>Cooking time(minutes):</span>
+          <input 
+            type="number"
+            onChange={(e)=>setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+            />
+        </label>
+
+      <button className='btn'>submit</button>
+
+      </form>
       
     </div>
   )
