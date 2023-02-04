84d66a03dca7cc24ca7e1e3da26ed408046aac65
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-04 16:20:07 +0530

    added open api key for generating images created routes for post request creating controller functions

---
+++ NodejsFCC/openaiclone/routes/openaiRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+router.post('/generateimage',(req,res)=>{
+    res.status(200).json({
+        success:true
+    })
+})
+
+module.exports = router
