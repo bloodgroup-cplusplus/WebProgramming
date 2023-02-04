9ab00c87fb412838a6ca9c286751d92529b5a290
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-04 14:16:36 +0530

    created css for the display of recipes

--- ReactProjectBasedLearning/UdemyCourse/recepie_project/src/pages/recipe/Recipe.js
+++ ReactProjectBasedLearning/UdemyCourse/recepie_project/src/pages/recipe/Recipe.js
@@ -15,7 +15,18 @@ function Recipe() {
     <div className='recipe'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'> Loading ...</p>}
-      {recipe && <h1>{recipe.title}</h1>}
+      {recipe && (
+
+        <>
+          <h2 className='page-tile'>{recipe.tile}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing=><li key={ing}>{ing}</li>)}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+
+        </>
+      )}
       
       
     </div>
