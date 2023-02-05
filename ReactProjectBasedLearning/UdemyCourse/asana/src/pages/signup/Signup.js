ca0679b5a087a2d62138bfdd35d40ca914cd9749
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-06 00:35:18 +0530

    added css to form files

--- ReactProjectBasedLearning/UdemyCourse/asana/src/pages/signup/Signup.js
+++ ReactProjectBasedLearning/UdemyCourse/asana/src/pages/signup/Signup.js
@@ -4,11 +4,92 @@ import './Signup.css'
 
 
 import React from 'react'
+import {useState} from "react"
 
 export default function Signup() {
+  const [email,setEmail] = useState("")
+  const[password,setPassword] =useState("")
+  const[displayName,setDisplayName] = useState(null)
+  const[dob,setDob] =useState("")
+  const[phone,setPhone] = useState("")
+  const [district,setDistrict] = useState("")
   return (
-    <div>
-     Signup 
-    </div>
+    <form className='auth-form'>
+      <h2>Sign up </h2>
+      <label>
+
+        <span>email:</span>
+        <input 
+          required
+          type="email"
+          onChange={(e)=>setEmail(e.target.value)}
+          value={email}
+          />
+      </label>
+
+
+      <label>
+
+        <span> password</span>
+        <input 
+          required 
+          type="password"
+          onChange={(e)=>setPassword(e.target.value)}
+          value={password}
+          />
+      </label>
+
+      <label>
+
+        <span> name:</span>
+        <input 
+        required 
+        type="text"
+        onChange={(e)=>setDisplayName(e.target.value)}
+        value={displayName}
+        />
+      </label>
+
+      <label>
+        <span> Date of birth:</span>
+        <input 
+        required 
+        type="date"
+        onChange={(e)=>setDob(e.target.value)}
+        value={dob}
+        />
+
+      </label>
+
+      <label>
+        <span>Phone:</span>
+        <input 
+        required 
+        type="number"
+        onChange={(e)=>setPhone(e.target.value)}
+        value={phone}
+        />
+      </label>
+
+      <label>
+        <span> District:</span>
+        <select required onChange={(e)=>setDistrict(e.target.value)} value={district}>
+          <option value="Gangtok">Gangtok</option>
+          <option value="Pakyong">Pakyong</option>
+          </select>
+        
+      </label>
+
+      <label>
+        <span> Profile Photo:</span>
+        <input 
+        required 
+        type="file"
+        />
+      
+      </label>
+
+    <button className='btn'>Continue to Razorpay</button>
+    </form>
   )
 }
