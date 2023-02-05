2f845ed68361d4e03f787278fa7e0b7a294e0a8d
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-05 23:53:36 +0530

    housekeeping code

--- ReactProjectBasedLearning/UdemyCourse/asana/src/pages/create/Create.js
+++ ReactProjectBasedLearning/UdemyCourse/asana/src/pages/create/Create.js
@@ -1,9 +1,12 @@
 import "./Create.css"
+import Sidebar from "../../components/Sidebar"
 
 
 export default function Create() {
   return (
     <div>
+        <Sidebar/>
+        Create
       
     </div>
   )
