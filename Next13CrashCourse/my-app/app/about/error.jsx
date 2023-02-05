5c5a46995b3e32ff5dd943033dcec2f170a08f1a
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-05 08:52:41 +0530

    installed and configured tailwind

--- Next13CrashCourse/my-app/app/about/error.jsx
+++ Next13CrashCourse/my-app/app/about/error.jsx
@@ -5,7 +5,7 @@ export default function Error({error,reset})
     return (
         <div>
             This ain't loading up:{error.message}
-            <button onClick={()=>reset()}></button>
+            <button onClick={()=>reset()}>Reload</button>
         </div>
     )
 }
