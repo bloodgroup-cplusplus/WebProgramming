ab291906bcfef070bfcdd0ea38e85edd460efc27
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-05 09:52:55 +0530

    fetching data from api

---
+++ Next13CrashCourse/my-app/app/movie.jsx
@@ -0,0 +1,9 @@
+export default function Movie({title})
+{
+    return (
+        <div>
+            <h1>{title}</h1>
+            <h2>Release date</h2>
+        </div>
+    )
+}
--- Next13CrashCourse/my-app/app/page.js
+++ Next13CrashCourse/my-app/app/page.js
@@ -1,16 +1,31 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+// stuffs like useeffect usestate doesn't work in next js 
+// cause next js is a server rendered component 
+// we can enable client side features like use state and use effect using the keyword "use client"
 
+// the only time you want to "use client" is when you want to do useeffect usestate etc . 
 
+//"use client"
 
-
-
-
-
-export default function Home() {
+export default async function Home() {
+  var res;
+  try{
+      const data = await fetch (`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+       res= await data.json()
+      console.log(res)
+  }catch(e)
+  {
+    console.log(e)
+  }
   return (
     <main >
-    <h1 className='text-lg py-2 m-4'>Hello next 13 </h1>
+    <h1 className='text-lg py-2 '>Hello next 13 </h1>
+    {res.results.map(movie=>(
+      <div>
+        <h1>{movie.title}</h1>
+      </div>
+    ))}
     </main>
   )
 }
