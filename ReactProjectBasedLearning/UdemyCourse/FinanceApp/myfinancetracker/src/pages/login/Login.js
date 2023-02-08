b43365c2f5eb5875682df96d3f1fbcc31da97e93
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-08 14:24:22 +0530

    login user hook

--- ReactProjectBasedLearning/UdemyCourse/FinanceApp/myfinancetracker/src/pages/login/Login.js
+++ ReactProjectBasedLearning/UdemyCourse/FinanceApp/myfinancetracker/src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import {useState} from "react"
+import {useLogin} from "../../hooks/useLogin"
 
 import styles from "./Login.modules.css"
 
@@ -7,9 +8,11 @@ import React from 'react'
 export default function Login() {
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
+  const{login,error,isPending} = useLogin()
+
   const handleSubmit=(e)=>{
     e.preventDefault()
-    console.log(email,password)
+    login(email,password)
   }
   return (
     <form onSubmit={handleSubmit} className={styles["login-form"]}>
@@ -31,7 +34,9 @@ export default function Login() {
           />
       </label>
       
-      <button className="btn">Login </button>
+      {!isPending && <button className="btn">Login </button>}
+      {isPending && <button className="btn" disabled>Loading</button>}
+      {error && alert(error)}
      </form>
     
   )
