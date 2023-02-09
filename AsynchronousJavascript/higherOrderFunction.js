0ef28c42517856188da4c1fa442574b5928a8582
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-09 22:35:50 +0530

    examples of callbacks

---
+++ AsynchronousJavascript/higherOrderFunction.js
@@ -0,0 +1,18 @@
+//let's setup a function 
+
+let logCall = function ()
+{
+    console.log("Logcall was called back")
+};
+
+
+// this is the callbackear
+setTimeout(function(){
+    console.log("the function was called back");
+},3000);
+
+// anyonymous way of writing it 
+
+// the console.log is executed after 3 seconds 
+
+// this is similar to click event in html 
