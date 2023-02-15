ee9c21197ad5b7c7a1194c254a7079708d0225b6
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-15 22:27:28 +0530

    added starter files alongwith firebase configuration

---
+++ Svelte/SVFIRE/sveltekit-auth/src/lib/firebase/firebase.client.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { deleteApp, getApps, initializeApp,getApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import {getAuth,setPersistence, inMemoryPersistence} from "firebase/auth"
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_APIKEY, 
+  authDomain:import.meta.env.VITE_AUTHDOMAIN, 
+  projectId:import.meta.env.VITEPROJECTID, 
+  storageBucket:import.meta.env.STORAGEBUCKET, 
+  messagingSenderId:import.meta.env.MESSAGINGSENDERID, 
+  appId:import.meta.env.APPID
+};
+
+// Initialize Firebase
+let firebaseApp;
+// this is due to the way svelte initializez firebase
+if(!getApps().length)
+{
+    // if that array is empty 
+    // we say 
+    // if that 's not true
+    firebaseApp=initializeApp(firebaseConfig)
+}
+
+else{
+    // if we already have firebase app initialized 
+    firebaseApp=getApp()
+    deleteApp(firebaseApp)
+    firebaseApp=initializeApp(firebaseConfig)
+}
