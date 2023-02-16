5abcbfc59a97d553dfe41db2c9c503e747b5d4e8
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-16 08:48:01 +0530

    navbar and react-icons

--- ReactProjectBasedLearning/UdemyCourse/TailwindAndReact/tailwindreact/src/components/Navbar.js
+++ ReactProjectBasedLearning/UdemyCourse/TailwindAndReact/tailwindreact/src/components/Navbar.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='text-white'>      
-        <h1>React</h1>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>      
+        <h1 className='w-full text-3xl font-bold text-[#fffff]'>React</h1>
+        <ul className='flex'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>Signup</li>
+            <li className='p-4'>Login</li>
+            <li className='p-4'>Feed</li>
+            <li className='p-4'>Donate</li>
+        </ul>
 
     </div>
   )
