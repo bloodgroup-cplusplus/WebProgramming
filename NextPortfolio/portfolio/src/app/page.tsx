23dea3e24cd9a683efdf845d5e790da0ac20296a
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-23 10:22:42 +0530

    added react simple typewriter with snap center and other features

--- NextPortfolio/portfolio/src/app/page.tsx
+++ NextPortfolio/portfolio/src/app/page.tsx
@@ -7,9 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Header/>
-      <section id="hero">
+      <section id="hero" className='snap-center'>
         <Hero/>
       </section>
       {/* Hero */}
--- NextPortfolio/portfolio/src/components/Hero.tsx
+++ NextPortfolio/portfolio/src/components/Hero.tsx
@@ -1,9 +1,25 @@
+"use client"
 import React from 'react'
+import {Cursor,useTypewriter} from "react-simple-typewriter"
+import BackgroundCircles from "./BackgroundCircles"
+
 
 type Props = {}
 
 export default function Hero({}: Props) {
+    const [text,count] = useTypewriter({
+        words:["Brand","Design","Develop","Deploy"],
+        loop:true,
+        delaySpeed:2000,
+    });
   return (
-    <div>Hero</div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles/>
+        <h1>
+          <span >{text}</span> 
+            <Cursor cursorColor='#F7AB0A'/>
+        </h1>
+
+    </div>
   )
 }
