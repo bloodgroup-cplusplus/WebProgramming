27d3f9bdbbb3d0c92fcc7148b82fe20bd9c17b0a
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-23 08:37:10 +0530

    added tailwind and starter code

---
+++ NextPortfolio/portfolio/src/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-blue-500'>Lets build an Awesome portfolio</h1>
+      {/* Header */}
+      {/* Hero */}
+      {/* About */}
+      {/* Experience */}
+      {/* Skills */}
+      {/* Projects */}
+      {/*Contact Me */}
+
+
+
+    </div>
+    
+  )
+}
