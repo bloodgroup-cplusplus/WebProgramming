148c6c11f0c6cb35af2fba5e9ae7646486cb2a4b
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-25 07:29:39 +0530

    client socket program in c

---
+++ Socket_Programming_In_C/tcp_client.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// we include types and socket for definition of socket functions that we are going to be using 
+// for our sockets 
+
+
+#include<sys/types.h>
+#include<sys/socket.h>
+
+// to include address information we need in . net 
+
+#include<netinet/in.h>
+
+// then we have the main function 
+
+int main()
+{
+    // in this example we will start writing our tcp client 
+    // hold the socket descriptor 
+    int network_socket ;
+
+
+    return 0;
+}
