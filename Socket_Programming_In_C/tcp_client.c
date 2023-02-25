383ecdffe1fc1de4da6a788abb0640aaeaef3134
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-25 07:40:10 +0530

    connect and recv data in c client program

--- Socket_Programming_In_C/tcp_client.c
+++ Socket_Programming_In_C/tcp_client.c
@@ -20,6 +20,35 @@ int main()
     // hold the socket descriptor 
     int network_socket ;
 
+    // call the socket function and store the result of it in the integer 
+    network_socket=socket(AF_INET,SOCK_STREAM,0); // we will use 0 for tcp , if we use raw sockets we use other integer 
+
+    // this is actually all we need to do to create a socket 
+    // we need to write one of the server 
+    // to send and receive data from 
+    // we need to now connect it to anotehr socket 
+    // before we can get to connect 
+    // we should specify address to connect to 
+    // specify an address for the socket 
+    struct sockaddr_in server_address;
+    // af_inet 
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    // htons specifies the port to connect remotely 
+    // specify the ip address . 
+    // bind to any address on the local computer 
+    // shortcut really already there for us 
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    int connection_status= connect(network_socket,(struct sockaddr *) &server_address, sizeof(server_address));
+    // check for error with the connection 
+    if(connection_status ==-1)
+    {
+        printf("There was an error making a connection to the remote server");
+    }
+    // receive data from the server
+    char server_response[299] ;
+    recv(network_socket,&server_response,sizeof(server_response),0);
+
 
     return 0;
 }
