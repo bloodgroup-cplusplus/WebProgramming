b953b4b2e32db71baf3269a5c51be48895fa33c5
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-28 22:20:59 +0530

    added video

--- rds/src/App.jsx
+++ rds/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import logo from "./assets/logo.png"
+import homestay_video from "./assets/homestay.mp4"
+
 import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      Hello chad
+    <div className='video_frame'>
+      <video   controls autoPlay loop>
+        <source src={homestay_video} type="video/mp4">
+        </source>
+      </video>
+        <img src={logo} alt="homestay_logo" className='hero'></img>
+      </div>
      
-    </div>
   )
 }
 
