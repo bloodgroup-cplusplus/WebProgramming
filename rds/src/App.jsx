2a99270f1f0e804b880154c61ce6995bf8dba129
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-02-28 21:28:06 +0530

    added starter files

---
+++ rds/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import "./App.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Hello chad
+     
+    </div>
+  )
+}
+
+export default App
