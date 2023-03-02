2897c9c0361318c4c697f8bf0b56a7fb34f1f29c
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-02 21:48:07 +0530

    this added first intro

---
+++ Svelte/FCC/portfolio/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<h1> My first svelte component</h1>
+<p>Hello world </p>
+<div>
+<a href="https://youtube.com/c/lihautan">Link to my youtube</a>
+</div>
+
+
+<style>
+    h1{
+        color:redeccapurple;
+
+    }
+
+    p{
+        font-size:44px;
+    }
+</style>
+
+<script>
+    let name="lihau";
+    // you can write any javascript over here 
+
+</script>
