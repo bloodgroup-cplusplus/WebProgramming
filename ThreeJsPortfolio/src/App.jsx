4897ed2c99a2980041bb7af282ab20421f000b1d
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-03 17:47:31 +0530

    added section components to the main page

--- ThreeJsPortfolio/src/App.jsx
+++ ThreeJsPortfolio/src/App.jsx
@@ -1,12 +1,26 @@
-
+// import all of the components just created 
+import {BrowserRouter} from "react-router-dom";
+import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from "./components";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      3d developer portfolio
-
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary ">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
    
