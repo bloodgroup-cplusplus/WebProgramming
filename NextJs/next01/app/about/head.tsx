8133f8f0eb8fa6e09fb1cc087129ab14c7310444
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-13 14:00:51 +0530

    added loading using the suspense feature of react js

---
+++ NextJs/next01/app/about/head.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function head() {
+  return (
+    <>
+        <title>About Page</title>
+        <meta content='width=device-width, initial-scale=1' name="viewport"/>
+        <meta name="description" content="Chad"/>
+        <link rel="icon" href="/favicon.ico"/>
+    </>
+      
+  )
+}
