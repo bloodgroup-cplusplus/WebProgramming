94e1096a105daf76e87b9ec03fba63a1209b2e87
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-13 13:16:03 +0530

    create link to home page

---
+++ NextJs/next01/app/about/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function AboutLayout({
+    children,
+}:{
+    children:React.ReactNode
+})
+{
+  return (
+    <>
+    <nav>About Navbar</nav>
+    <main>
+        {children}
+    </main>
+    </>
+  )
+}
--- NextJs/next01/app/about/page.tsx
+++ NextJs/next01/app/about/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Link from "next/link"
 
 export default function About() {
   return (
+    <>
+
     <h1>About</h1>
+    <Link href="/">Link to home page</Link>
+    </>
   )
 }
