adb38b885765e20c668e32ae6a75e1a6f58befca
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-18 22:37:48 +0530

    added form to submit

--- NextJs/next_client_components/app/components/Navbar.tsx
+++ NextJs/next_client_components/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from "next/link"
+import Search from './Search'
 
 export default function Navbar() {
   return (
@@ -7,6 +8,7 @@ export default function Navbar() {
         <h1 className='text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0'>
             <Link href="/">WikiRocket!</Link>
         </h1>
+        <Search/>
     </nav>
       
   )
