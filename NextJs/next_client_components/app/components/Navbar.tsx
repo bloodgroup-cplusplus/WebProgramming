ff7ffc905f25f2e135cdfa5ca05d0321bd0476db
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-18 21:59:57 +0530

    added navbar

--- NextJs/next_client_components/app/components/Navbar.tsx
+++ NextJs/next_client_components/app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 export default function Navbar() {
   return (
-    <nav className=''>
-        <h1 className='text-3xl font-bold text-black grid place-content-center mb-2 md:mb-0'>
+    <nav className='bg-slate-600 p-4 flex justify-between flex-col md:flex-row stickey top-0 drop-shadow-xl'>
+        <h1 className='text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0'>
             <Link href="/">WikiRocket!</Link>
         </h1>
     </nav>
