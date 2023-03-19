ebce71ce47477e29a048d233a5867d96c73fe2d7
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-19 15:39:13 +0530

    created wiki scraper

--- NextJs/next_client_components/app/[searchTerm]/page.tsx
+++ NextJs/next_client_components/app/[searchTerm]/page.tsx
@@ -1,4 +1,5 @@
 // create a search results compoentn s
+import getWikiResults from '@/lib/getWikiResults'
 
 import React from 'react'
 
@@ -8,8 +9,21 @@ type Props = {
     }
 }
 
-export default function page({params:{searchTerm}}: Props) {
-  return (
-    <div>page</div>
-  )
+export default async function SearchResults({params:{searchTerm}}: Props) {
+    const wikiData:Promise<SearchResult> = getWikiResults(searchTerm)
+    const data=await wikiData
+    const results: Result[] | undefined = data?.query?.pages
+    const content = (
+        <main className='bg-slate-200 mx-auto max-w-lg py-1 min-h-screen'>
+            { results
+                ? Object.values(results).map(result => {
+                    return <p>{JSON.stringify(result)}</p>
+                })
+
+                :<h2 className='p-2 text-xl'>{`${searchTerm} Not found `}</h2>
+            }
+
+        </main>
+    )
+  return content
 }
---
+++ NextJs/next_client_components/types.d.ts
@@ -0,0 +1,17 @@
+type Result ={
+    pageid:string,
+    title:string,
+    extract:string,
+    thumbnail?:{
+        source:string,
+        width:number,
+        height:number,
+    }
+}
+
+
+type SearchResult = {
+    query?:{
+        pages?:Result[],
+    },
+}
