3afdcd63cdb99822d5a8c8ae66dee869dab6eb9f
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-21 12:55:59 +0530

    json error

---
+++ NextJs/fullstackwebnexttailwindpostgresprisma/app/api/hello/getPosts.tsx
@@ -0,0 +1,25 @@
+import type { NextApiRequest,NextApiResponse } from "next";
+import prisma from "../../../prisma/client"
+
+
+export default async function handler (
+  req:NextApiRequest,
+  res: NextApiResponse
+){
+  if(req.method === "GET")
+  {
+    try{
+      // get prisma to get the posts 
+      const data = await prisma.post.findMany()
+      return res.status(200).json(data)
+
+
+    }
+    catch(error)
+    {
+      return res.status(500).json(error)
+
+
+    }
+  }
+}
--- NextJs/fullstackwebnexttailwindpostgresprisma/app/page.tsx
+++ NextJs/fullstackwebnexttailwindpostgresprisma/app/page.tsx
@@ -1,7 +1,18 @@
 import Link from "next/link"
 
+async function getPosts()
+{
+  const res = await fetch(`${process.env.BASE_URL}/api/hello/getPosts`)
+  if(!res.ok)
+  {
+    console.log("Response")
+  }
+  return res.json()
+}
 
-export default function Home() {
+
+export default async function Home() {
+  const data = await getPosts()
   return (
     <main className= "py-7 px-48">
       <Link className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md" 
