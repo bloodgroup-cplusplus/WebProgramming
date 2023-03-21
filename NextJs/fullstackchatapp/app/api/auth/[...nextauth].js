335961e5b7ab9122a143c1a204533d30f7e206a2
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-21 17:05:19 +0530

    configured prisma adapters with next auth

--- NextJs/fullstackchatapp/app/api/auth/[...nextauth].js
+++ NextJs/fullstackchatapp/app/api/auth/[...nextauth].js
@@ -1,2 +1,17 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
+// install an adapter 
+// we need an easy way to setup the prisma models and make work with prisma models 
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../prisma/client"
+
+
+export default NextAuth({
+    adapter:PrismaAdapter(prisma),
+    providers:[
+        GoogleProvider({
+            clientId:process.env.GOOGLE_CLIENT_ID,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ]
+})
