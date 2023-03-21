8ec6c06a314408dc7529c7c2550edc208f213d0a
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-21 19:48:54 +0530

    added schema format from prisma

--- NextJs/fullstackchatapp/app/api/auth/[...nextauth].js
+++ NextJs/fullstackchatapp/app/api/auth/[...nextauth].js
@@ -12,6 +12,6 @@ export default NextAuth({
         GoogleProvider({
             clientId:process.env.GOOGLE_CLIENT_ID,
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ]
+        }),
+    ],
 })
