45b8ddb559483093c1002ec368ea009319837539
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-21 11:28:36 +0530

    install railway client using brew install railway perform railway login and railway link for postgres database api links

--- NextJs/fullstackwebnexttailwindpostgresprisma/app/page.tsx
+++ NextJs/fullstackwebnexttailwindpostgresprisma/app/page.tsx
@@ -1,10 +1,11 @@
-import styles from './page.module.css'
+import Link from "next/link"
 
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Home</h1>
+    <main className= "py-7 px-48">
+      <Link className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md" 
+      href="{/dashboard}"> Go to the dashboard</Link>
      
     </main>
   )
