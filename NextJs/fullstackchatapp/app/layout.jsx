756eb92952553ab3fc48adf226d77f12980b79ea
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-21 22:58:15 +0530

    added robot o and added screen fonts

--- NextJs/fullstackchatapp/app/layout.jsx
+++ NextJs/fullstackchatapp/app/layout.jsx
@@ -1,5 +1,13 @@
 import './globals.css'
 import Nav from './auth/Nav'
+import {Roboto} from "next/font/google"
+
+const roboto=Roboto({
+  subsets:["latin"],
+  weight:["400","700"],
+  variable: "--font-roboto",
+
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,9 +18,9 @@ export default function RootLayout({children})
 {
   return (
     <html lang="en">
-        <body>
-          {children}
+        <body className='mx-4 md:mx-48 xl:mx-96'>
           <Nav/>
+          {children}
           </body>
     </html>
   )
