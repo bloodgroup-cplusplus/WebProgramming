6b0c66b4aefc8f6cf893c03635b62625ae8d43f2
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-27 10:57:04 +0530

    add remark and remark-html using npm i remark remark-html for helping us convert the markdown files into html

--- NextJs/next13blog/app/posts/[postId]/page.tsx
+++ NextJs/next13blog/app/posts/[postId]/page.tsx
@@ -1,6 +1,24 @@
 import { getSortedPostsData } from "@/lib/posts"
 import {notFound} from "next/navigation"
 
+
+export async function generateMetadata({params}:{params:{postId:string}})
+{
+    const posts = getSortedPostsData()
+    const {postId} = params 
+    const post = posts.find(post=>post.id === postId)
+
+    if(!post)
+    {
+        return {
+            title:"Post not found"
+        }
+    }
+    return {
+        title:post.title
+    }
+}
+
 export default async function Post({params}:{params:{postId:string}})
 {
     const posts = getSortedPostsData() // deduped 
