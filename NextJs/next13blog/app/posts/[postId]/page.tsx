a8c3e395ada57881e580f6a32dda3d7ff4fa1598
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-27 17:11:57 +0530

    project build successful

--- NextJs/next13blog/app/posts/[postId]/page.tsx
+++ NextJs/next13blog/app/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
-import { getSortedPostsData } from "@/lib/posts"
+import getFormattedDate from "@/lib/getFormattedDate"
+import { getPostData, getSortedPostsData } from "@/lib/posts"
 import {notFound} from "next/navigation"
+import Link from "next/link"
 
 
 export async function generateMetadata({params}:{params:{postId:string}})
@@ -34,7 +36,20 @@ export default async function Post({params}:{params:{postId:string}})
         return notFound()
     }
 
+    const {title, date, contentHtml} = await getPostData(postId)
+
+    const pubDate = getFormattedDate(date)
+
     return (
-        <div>Page</div>
+        <main className="px-6 prose prose-xl prose-slate dark: prose-invert mx-auto">
+            <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+            <p className="mt-0">{pubDate}</p>
+            <article>
+                <section dangerouslySetInnerHTML={{ __html:contentHtml}}/>
+                <p>
+                    <Link href="/">Back to home</Link>
+                </p>
+            </article>
+        </main>
     )
 }
