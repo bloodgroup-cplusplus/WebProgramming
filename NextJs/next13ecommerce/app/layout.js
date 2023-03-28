67e95458f0e4fb92d7b7fc2a10678a9ca297c3a1
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-28 10:13:56 +0530

    added stripe and environmental variables

--- NextJs/next13ecommerce/app/layout.js
+++ NextJs/next13ecommerce/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import {Inter} from "next/font/google"
+import Link from "next/link"
 
 const inter = Inter({subsets:['latin']})
 
@@ -12,13 +13,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" /> 
       </head>
-        
-
       <body className={'min-h-screen flex flex-col relative'+ inter.className}>
       <header className='sticky top-0 p-6 bg-white border-b border-solid border-blue-900
-      shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8'> Chad</header>
+      shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8 flex item-center justify-between'>
+        <Link href = {"/"}>
+        <h1 className='uppercase cursor-pointer hover:'>Fruit Shop</h1>
+        </Link>
+        <i className="fa-solid cursor-pointer hover:text-slate-500 fa-cart-shopping"></i>
+      </header>
         <div className='flex-1'>
           {children}
         </div>
--- NextJs/next13ecommerce/app/page.js
+++ NextJs/next13ecommerce/app/page.js
@@ -1,10 +1,24 @@
 import Image from 'next/image'
+import Stripe from 'stripe'
 
+
+async function getStripeProducts()
+{
+    const stripe = new Stripe(process.env.STRIPE_SECRET ?? '',{
+      apiVersion:'2020-08-27'
+
+    })
+    const res = await stripe.prices.list({
+      expand:['data-product']
+    })
+    const prices= res.data 
+    return prices
+
+}
 
 export default function Home() {
   return (
-    <main className=''> 
-      YO THANKS 
+    <main className='p-4'> 
     </main>
   )
 }
