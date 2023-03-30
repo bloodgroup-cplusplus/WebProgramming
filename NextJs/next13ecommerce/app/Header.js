d97281ad114f0a12bd217761a82e242d35df74e9
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-30 19:45:27 +0530

    open and close cart

--- NextJs/next13ecommerce/app/Header.js
+++ NextJs/next13ecommerce/app/Header.js
@@ -7,7 +7,7 @@ import Modal from './Modal'
 export default function Header() {
     const cartItems = useCart(state => state.cart)
     const openModal = useCart(state => state.openModal)
-    const setOpenModal = useCart(state => state.setOpenModal)
+    const setOpenModal = useCart(state=> state.setOpenModal)
     console.log(cartItems)
     return (
         <header className='sticky top-0 p-6 bg-white border-b border-solid border-blue-900 shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8 flex item-center justify-between'>
--- NextJs/next13ecommerce/app/Modal.js
+++ NextJs/next13ecommerce/app/Modal.js
@@ -1,10 +1,23 @@
 "use client"
 import React from 'react'
+import useCart from './(store)/store'
 import ReactDom from "react-dom"
 
 export default function Modal() {
+  const closeModel = useCart(state=>state.setOpenModal)
   return ReactDom.createPortal(
-    <div>Modal</div>,
+    <div className='fixed top-0 left-0 w-screen h-screen z-50'>
+      <div onClick={closeModel} className='bg-transparent absolute inset-0'>
+        <div className='flex flex-col bg-white absolute right-0 top-0 h-screen shadow-lg w-screen sm:w-96 max-w-screen gap-4'>
+          <div className='flex items-center justify-between text-xl relative'>
+            <h1>Cart</h1>
+            <i onClick={closeModal} className='fa-solid cursor-pointer hover:opacity-60 fa-xmark'></i>
+            <div className='absolute bottom-0 left-1/2 -translate-x-1/2 h-[1px] bg-slate-300 w-2/3'></div>
+          </div>
+          
+        </div>
+      </div>
+    </div>,
     document.getElementById("portal")
     
   )
