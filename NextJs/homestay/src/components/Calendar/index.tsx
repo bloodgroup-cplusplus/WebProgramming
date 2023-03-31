b73a719f8695c2256f2e01b038f148d32648ff34
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-31 13:14:39 +0530

    added date-fns for date react-calendar and types

--- NextJs/homestay/src/components/Calendar/index.tsx
+++ NextJs/homestay/src/components/Calendar/index.tsx
@@ -1,13 +1,29 @@
+"use client"
 import {FC} from "react"
+import { useState } from "react"
 import ReactCalendar from "react-calendar"
 
 interface indexProps{}
+interface DateType{
+    justDate: Date | null 
+    dateTime: Date | null
+}
 
 const index:FC<indexProps> = ({}) =>{
+    const[date, setDate] = useState<DateType>({
+        justDate:null, 
+        dateTime:null,
+    })
     return (
-        <div>
+        <div className="h-screen flex flex-col justify-center items-center">
+            {date.justDate?(
+                <div className="flex gap-4">
+
+                </div>
+            ):(
             <ReactCalendar minDate={new Date()}
-            className="REACT-CALENDAR p-2" view="month" onClickDay={(date)=>console.log(date)}/>
+            className="REACT-CALENDAR p-2" view="month" onClickDay={(date)=> setDate((prev)=>({...prev,justDate:date}))}/>
+            )}
         </div>
     )
 }
