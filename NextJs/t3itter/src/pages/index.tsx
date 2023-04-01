5968c21b5eba66e9108e3e680f41cc94c5852b91
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-01 11:37:44 +0530

    Next image tag, style up

--- NextJs/t3itter/src/pages/index.tsx
+++ NextJs/t3itter/src/pages/index.tsx
@@ -3,7 +3,8 @@ import Head from "next/head";
 import { SignOutButton} from "@clerk/nextjs"
 import { SignInButton } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
-import { RouterOutputs } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
+import Image from "next/image";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime"
 dayjs.extend(relativeTime)
@@ -21,7 +22,7 @@ const CreatePostWizard =() =>{
 
   return (
     <div  className = "flex gap-3  w-full">
-    <img src ={user.profileImageUrl} alt="Profile image" className="h-14 w-14 rounded-full"/>
+    <Image src ={user.profileImageUrl} alt="Profile image" className="h-14 w-14 rounded-full" width={56} height={56}/>
     <input placeholder="Type some emojis!" className="grow bg-transparent outline-none"/>
   </div>
   );
@@ -37,7 +38,9 @@ const PostView = (props:PostWithUser)=>{
 
   return (
     <div key = {post.id} className=" flex gap-3 border-b border-slate-400 p-4">
-      <img src={author.profileImageUrl} className="rounded-full h-14 w-14" />
+      <Image src={author.profileImageUrl} className="rounded-full h-14 w-14" alt={`@${author.username}'s profile picture`}
+      width={56} height = {56}  />
+
       <div className="flex flex-col">
         <div className="flex gap-1  text-slate-300">
           <span>{author.username}</span>
