b668ccc06033836fdf9aaa52a8d0c1faaa90bd3b
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-01 14:30:47 +0530

    dayjs implemented

--- ReactProjectBasedLearning/FlowBite/my-project/src/Pages/ListGriviences.js
+++ ReactProjectBasedLearning/FlowBite/my-project/src/Pages/ListGriviences.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import dayjs from 'dayjs'
+import relativeTime from "dayjs/plugin/relativeTime"
+dayjs.extend(relativeTime)
 //import Comments from "./Comments"
 
 const ListGriviences = ({griviences,user}) => {
@@ -46,7 +48,7 @@ return (
                   <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
                   {grivience['schoolName']}
                   </span>
-                  <span class="text-sm">{grivience['createdAt'].toDate().toString().substring(3,25)}</span>
+                  <span class="text-sm">{dayjs(grivience['createdAt'].toDate().toString().substring(3,25)).fromNow()}</span>
               </div>
               <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{'Grivience heading goes here'}</h2>
               <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{grivience['grivience']}</p>
