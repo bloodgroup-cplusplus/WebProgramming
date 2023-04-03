5a0c0048dea95e6b733a1f26308eece088e80990
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-03 19:08:04 +0530

    run with turbopack

--- NextJs/notes/app/page.tsx
+++ NextJs/notes/app/page.tsx
@@ -5,9 +5,6 @@ export default function HomePage() {
     <div>
       <h1>Home Page</h1>
       <p>Some content</p>
-      <p>Some content again</p>
-      <p>Lets add some content again </p>
-      <p>Lets build a tension</p>
     </div>
   ); 
 }
