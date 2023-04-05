808d36f0b6d5548e0174a0668555c2c609d76f66
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-05 12:41:48 +0530

    added static routes

---
+++ NextJs/notes/app/notes/[id]/page.tsx
@@ -0,0 +1,20 @@
+async function getNote(noteId:string)
+{
+    const res = await fetch ("");
+    {
+        next:{revalidate:10},
+    }
+
+
+
+
+export default async function NotePage()
+
+{
+    return (
+        <div>
+            <h1>notes</h1>
+        </div>
+    )
+
+}
