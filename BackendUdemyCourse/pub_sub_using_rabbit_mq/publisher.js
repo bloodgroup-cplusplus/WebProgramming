41d4da2f43c4d8e809780b1fa2b205645f7e253f
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-05 15:27:39 +0530

    added a bunch of stuff

--- BackendUdemyCourse/pub_sub_using_rabbit_mq/publisher.js
+++ BackendUdemyCourse/pub_sub_using_rabbit_mq/publisher.js
@@ -1,3 +1,26 @@
 /* Rabbit mq */
 
 const amqp = require("amqplib")
+
+const msg = {number:process.argv[2]}
+connect()
+
+async function connect()
+{
+    try{
+        const ampqServer = "amqps://gkyvbdpm:bj8eH5KTBLKnmnWfHNZKIjFk4TJsfTVh@puffin.rmq2.cloudamqp.com/gkyvbdpm"
+        const conection = await amqp.connect(ampqServer)
+        const channel = await connection. createChannel();
+        await channel.assertQueue("jobs"); // new queueu called jobs
+        await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)))
+        console.log(`Job sent successfully ${msg.number}`); 
+        await channel.close(); 
+        await connection.close();
+    }
+
+
+    catch(ex)
+    {
+        console.error(ex);
+    }
+}
