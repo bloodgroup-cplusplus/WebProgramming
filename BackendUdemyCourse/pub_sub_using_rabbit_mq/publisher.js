fc4465cfe3c7cd9d333ebba8e8afd3325d97c521
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-05 15:28:51 +0530

    fixed a spelling typo also to install rabbit mq playground using npm i amqplib

--- BackendUdemyCourse/pub_sub_using_rabbit_mq/publisher.js
+++ BackendUdemyCourse/pub_sub_using_rabbit_mq/publisher.js
@@ -9,7 +9,7 @@ async function connect()
 {
     try{
         const ampqServer = "amqps://gkyvbdpm:bj8eH5KTBLKnmnWfHNZKIjFk4TJsfTVh@puffin.rmq2.cloudamqp.com/gkyvbdpm"
-        const conection = await amqp.connect(ampqServer)
+        const connection = await amqp.connect(ampqServer)
         const channel = await connection. createChannel();
         await channel.assertQueue("jobs"); // new queueu called jobs
         await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)))
