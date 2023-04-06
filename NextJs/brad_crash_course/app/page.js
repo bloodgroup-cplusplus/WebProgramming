e971d09d826e25c836d5276f59662500cd42307e
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-06 08:28:09 +0530

    adding simple routes

--- NextJs/brad_crash_course/app/page.js
+++ NextJs/brad_crash_course/app/page.js
@@ -1,9 +1,25 @@
 // rafce is react arrow functions component export
 import React from 'react'
+import Link from 'next/link'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <h1>Welcom to Chad's Media</h1>
+      <ul>
+        <li>
+          <Link href ="/">Home</Link>
+        </li>
+
+        <li>
+          <Link href = "/about">About</Link>
+        </li>
+
+        <li>
+          <Link href = '/about/'>Team</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
