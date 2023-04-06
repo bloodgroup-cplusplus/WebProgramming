badf9c2f87454d0fd07ab7782229f0ab7766cc62
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-06 08:43:31 +0530

    adding custom fonts

--- NextJs/brad_crash_course/app/layout.jsx
+++ NextJs/brad_crash_course/app/layout.jsx
@@ -1,4 +1,12 @@
+
+import {Poppins} from "next/font/google"
 import './globals.css'
+
+
+const poppins = Poppins({
+  weight:['400','700'],
+  subsets:['latin'],
+})
 
 export const metadata = {
   title: 'Chad Media',
@@ -9,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
