4e6209948ce0876487ff306363e2678d681eefa1
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-07 22:04:55 +0530

    added courses api

--- NextJs/brad_crash_course/app/code/repos/page.jsx
+++ NextJs/brad_crash_course/app/code/repos/page.jsx
@@ -5,8 +5,14 @@ import {FaStar,FaCodeBranch,FaEye} from "react-icons/"
 async function fetchRepos()
 
 {
-    const response = await fetch ('https://api.github.com/users/bradtraversy/repos')
-    await new Promise((resolve)=>setTimeout(resolve,1000));//
+    const response = await fetch ('https://api.github.com/users/bradtraversy/repos',{
+    next:{
+        revalidate:60 // number of seconds it has to wait to revalidate new data
+        // if you are wanting to do it with dynamic changing data we do it 60 as how often are you adding the 
+        // new data or new repo
+
+    }})
+    await new Promise((resolve)=>setTimeout(resolve,1000));// wait one second
     const repos = await response.json();
     return repos;
 
@@ -22,23 +28,25 @@ const ReposPage =  async () =>{
     // since this is a server component so on doing console.log we see it on the server side code only 
     // the code is not reflected on the client side browser console
     // if we say " use client on the top and then console log then it's going to be put on the browser"
-    console.log(repos)
-    return <div className="repos-container">
-        <h2>Repositories</h2>
-        <ul className="repo-list1">
-            {repos.map((repo)=>(
-                <li key = {repo.id} >
-                <Link href = {`/code/repos/${repo.name}`}>
-                    <h3>{repo.name} </h3>
-                    <div className="repo-details">
-                        <span>
-                        </span>
-                    </div>
-                </Link>
-                </li>
-            ))}
-        </ul>
-    </div>
-}
 
-export default ReposPage
+        return (
+          <div className='repos-container'>
+            <h2>Repositories</h2>
+            <ul className='repo-list'>
+              {repos.map((repo) => (
+                <li key={repo.id}>
+                  <Link href={`/code/repos/${repo.name}`}>
+                    <h3>{repo.name}</h3>
+                    <p>{repo.description}</p>
+                    <div className='repo-details'>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      };
+
+export default ReposPage;
+   
---
+++ NextJs/brad_crash_course/app/components/RepoDirs.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Link from "next/link"
+
+async function fetchRepoContents(name)
+{
+    await new Promise((resolve)=> setTimeout(resolve,3000));
+    // it takes 3 seconds before it gives you our data 
+
+    const response = await fetch(
+        `https://api.github.com/repos/bradtraversy/${name}/contents`,
+        {
+            next:{
+                revalidate:60
+            }
+        }
+    );
+    const contents = await response.json()
+    return contents
+}
+
+const RepoDirs = async ({name}) => {
+    const contents = await fetchRepoContents(name)
+    const dirs = contents.filter((content)=>content.type === "dir");
+
+
+  return (
+    <>
+    <h3>Directories</h3>
+    <ul>
+        {dirs.map((dir)=>(
+            <li key = {dir.path}>
+                <Link href = {`/code/repos/${name}/${dir.path}`}>
+                    {dir.path}
+                </Link>
+            </li>
+        ))}
+    </ul>
+    </>
+  )
+}
+
+export default RepoDirs
