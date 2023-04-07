4264a3723142255edde9a45431d5c4d50d2bc0c9
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-07 20:36:13 +0530

    added back button

--- NextJs/brad_crash_course/app/code/repos/[name]/page.jsx
+++ NextJs/brad_crash_course/app/code/repos/[name]/page.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Repo from '@/app/components/Repo'
+import Link from 'next/link'
 
 const RepoPage = ({params:{name}}) => {
   return (
     <div className='card'>
+        <Link href = "/code/repos" className='btn btn-back'>
+            Back To Repositories 
+            </Link>
         <Repo name={name}/>
     </div>
   )
