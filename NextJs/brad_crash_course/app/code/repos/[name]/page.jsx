61916dc1062c7ae52dd52efa23326a145c11088a
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-07 21:20:01 +0530

    learnt how to add suspense boundries

--- NextJs/brad_crash_course/app/code/repos/[name]/page.jsx
+++ NextJs/brad_crash_course/app/code/repos/[name]/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Repo from '@/app/components/Repo'
 import RepoDirs from '@/app/components/RepoDirs'
 import Link from 'next/link'
+import { Suspense } from 'react'
 
 const RepoPage = ({params:{name}}) => {
   return (
@@ -9,8 +10,12 @@ const RepoPage = ({params:{name}}) => {
         <Link href = "/code/repos" className='btn btn-back'>
             Back To Repositories 
             </Link>
+        <Suspense fallback={<div>Loading repo...</div>}>
         <Repo name={name}/>
+        </Suspense>
+        <Suspense fallback={<div>Loading directories...</div>}>
         <RepoDirs name ={name}/>
+        </Suspense>
     </div>
   )
 }
