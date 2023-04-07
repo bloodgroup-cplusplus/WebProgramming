a14353908325c5bc6b7df6ff59bbbe60bbedd665
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-07 09:07:57 +0530

    added bunch of comments

--- NextJs/brad_crash_course/app/code/repos/page.jsx
+++ NextJs/brad_crash_course/app/code/repos/page.jsx
@@ -1,5 +1,26 @@
-const ReposPage = () =>{
-    return <div>ReposPage</div>
+async function fetchRepos()
+
+{
+    const response = await fetch ('https://api.github.com/users/bloodgroup-cplusplus/repos')
+    const repos = await response.json();
+    return repos;
+
+    
+
+}
+
+
+
+
+const ReposPage =  async () =>{
+    const repos = await fetchRepos()
+    // since this is a server component so on doing console.log we see it on the server side code only 
+    // the code is not reflected on the client side browser console
+    // if we say " use client on the top and then console log then it's going to be put on the browser"
+    console.log(repos)
+    return <div>
+        {repos[0].name}
+    </div>
 }
 
 export default ReposPage
