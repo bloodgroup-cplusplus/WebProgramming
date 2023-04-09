08da11872b5de303d7c96d6a339fae55101154da
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-09 09:29:13 +0530

    added bunch of stuff

--- NextJs/next_fire/app/signup/page.jsx
+++ NextJs/next_fire/app/signup/page.jsx
@@ -26,7 +26,10 @@ function Page() {
         console.log(result)
         return router.push("/dashboard")
     }
-    return (
+
+    if (!user)
+    {
+        return (
         <section class="bg-gray-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
@@ -72,6 +75,13 @@ function Page() {
   </div>
 </section>
     );
+        }
+
+    else 
+    {
+        router.push("/dashboard")
+    }
+
 }
 
 export default Page;
