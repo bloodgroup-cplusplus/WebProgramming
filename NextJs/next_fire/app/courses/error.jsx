2b5228083975ff420cb93d2a48865bf975e2b5ee
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-09 13:44:02 +0530

    added departments page

--- NextJs/next_fire/app/courses/error.jsx
+++ NextJs/next_fire/app/courses/error.jsx
@@ -1,5 +1,5 @@
+'use client'
 import React from 'react'
-
 const error = () => {
   return (
     <div>
--- NextJs/next_fire/app/courses/loading.jsx
+++ NextJs/next_fire/app/courses/loading.jsx
@@ -1,5 +1,5 @@
+'use client'
 import React from 'react'
-
 const loading = () => {
   return (
     <div>
