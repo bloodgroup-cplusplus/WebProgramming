382f3ba3e6f7d9b6416c083be924118e38566ba8
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-09 17:27:33 +0000

    playing with colors

--- NextJs/next_fire/app/components/Tadong.jsx
+++ NextJs/next_fire/app/components/Tadong.jsx
@@ -19,7 +19,7 @@ The college strives to cater to the growing need of quality education in Sikkim.
 
 </p>
 
-<Link href= "/courses" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-green-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+<Link href= "/courses" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-green-500 hover:bg-primary-800 focus:ring-green-400 focus:ring-green-400 dark:focus:ring-primary-900">
                 View Courses
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </Link>
