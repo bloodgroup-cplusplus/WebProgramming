a0e5ce161decabab109e508ba2233062aa7e7017
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-09 14:39:55 +0000

    changed bunch of class to classname

--- NextJs/next_fire/app/signup/page.jsx
+++ NextJs/next_fire/app/signup/page.jsx
@@ -43,7 +43,7 @@ function Page() {
               <form class="space-y-4 md:space-y-6" onSubmit={handleForm}>
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
                   </div>
                   <div>
                       <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Full Name</label>
@@ -65,9 +65,9 @@ function Page() {
                         <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-green-500 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                       </div>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-green-500 focus:outline-green-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-green-500 dark:focus:ring-primary-800">Create an account</button>
+                  <button type="submit" class="w-full text-white bg-green-500 hover:bg-primary-700 focus:ring-green-500 focus:outline-green-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-green-500 dark:focus:ring-primary-800">Create an account</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <a href="#" class="font-medium text-green-500 hover:underline dark:text-primary-500">Login here</a>
+                      Already have an account? <a href="/signin" class="font-medium text-green-500 hover:underline dark:text-primary-500">Login here</a>
                   </p>
               </form>
           </div>
