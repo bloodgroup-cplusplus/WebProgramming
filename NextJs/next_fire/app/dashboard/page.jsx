3a616f71055ad4cf04655371980e5c48ae548ef7
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-10 19:27:36 +0530

    added Namchi component

--- NextJs/next_fire/app/dashboard/page.jsx
+++ NextJs/next_fire/app/dashboard/page.jsx
@@ -5,7 +5,7 @@ import { useAuthContext } from "../context/AuthContext"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 import Signout from "../firebase/auth/signout"
-import Tadong from "../components/Tadong"
+import Namchi from "../components/Namchi"
 import Burtuk from "../components/Burtuk"
 
 
@@ -41,7 +41,7 @@ function Page()
         <>
             <h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Choose Your College Here</h1>
             <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Governement of Sikkim has 11 outstanding affliated under Sikkim University. All colleges equipped with world class infrastructure and facilities ensures a flying start to your career. </p> 
-            <Tadong/>
+            <Namchi/>
             <Burtuk/>
         </>
 
