18e410debc9a13d074ea169b6be49ade5492fb8b
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-11 09:25:02 +0530

    added bunch of stuffs

--- NextJs/next_fire/app/components/Navbar.jsx
+++ NextJs/next_fire/app/components/Navbar.jsx
@@ -4,8 +4,10 @@ import logo from "../Images/Seal_of_Sikkim.png"
 import Link from 'next/link'
 import {useAuthContext} from "../context/AuthContext"
 import  SignOut  from "../firebase/auth/signout" 
+import { useRouter } from 'next/navigation'
 
 const Navbar = () => {
+  const router = useRouter()
   const handleSignout=()=>{
     const{result,error} = SignOut() 
     if(error)
@@ -13,6 +15,8 @@ const Navbar = () => {
       console.log('signout error')
 
     }
+    router.push("/")
+
   
   }
   const {user} = useAuthContext()
@@ -22,14 +26,14 @@ const Navbar = () => {
   return (
     <nav className="bg-green-500 border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Image src={logo} height={100} width={100} alt = "Government Of Sikkim Logo" />
-        <span className="self-center text-semibold text-white font-medium whitespace-nowrap dark:text-white">Government of Sikkim</span>
+        <Image src={logo} height={60} width={60} alt = "Government Of Sikkim Logo" />
+        <span className="self-center text-sm text-semibold text-white font-light dark:text-white">Government of Sikkim</span>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
       <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-green-500 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-green-500 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="font-light text-sm flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-green-500 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-green-500 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
           <Link  href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
         </li>
