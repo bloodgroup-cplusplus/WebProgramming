d7ca5f39939cdd3ba26bac777d7293db2cc28f47
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-12 10:27:56 +0530

    added bunch of stuff

--- NextJs/next_fire/app/components/Courses.jsx
+++ NextJs/next_fire/app/components/Courses.jsx
@@ -25,10 +25,10 @@ const Courses = () => {
                           Dr.Somedean Genericsurname (HOD)
                       </span>
                   </div>
-                  <a href="#" className="inline-flex items-center font-medium text-green-500 dark:text-primary-500 hover:underline">
+                  <Link href="/apply" className="inline-flex items-center font-medium text-green-500 dark:text-primary-500 hover:underline">
                       Apply
                       <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                  </a>
+                  </Link>
               </div>
           </article> 
           <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -42,7 +42,7 @@ Our faculty is passionate about teaching and research, and they bring their expe
 
 Join us for a rewarding journey in History and discover how the study of the past can help you understand the present and shape the future!</p>              <div className="flex justify-between items-center">
                   <div className="flex items-center space-x-4">
-                      <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
+                      <Image className=" rounded-full" width={36} height={36} src={prof} alt="Bonnie Green avatar" />
                       <span className="text-xs font-medium dark:text-white">
                           Dr. Somedean Again (HOD)
                       </span>
