609876e78b3e07adec846f26f40374591512eaa7
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-12 10:43:25 +0530

    added bunch of stuff

--- NextJs/next_fire/app/components/Navbar.jsx
+++ NextJs/next_fire/app/components/Navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
   
   }
   const {user} = useAuthContext()
+  console.log(user)
   if(!user)
   {
 
@@ -68,20 +69,15 @@ const Navbar = () => {
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="font-light flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-green-500 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-green-500 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <button  onClick={handleSignout} className="inline-flex items-center justify-center px-3 py-4 mr-3 text-base text-py-7 font-light text-center text-white rounded-lg bg-green-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+        <li>
+          <a href="#" className="block py-2 pl-3 pr-4 text-white rounded md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Hi {user.uid}</a>
+        </li>
+        <button  onClick={handleSignout} className="inline-flex items-center justify-center px-3 py-0 mr-1 text-base text-py-7 font-light text-center text-white rounded-lg bg-green-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Signout
                 <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
 
-        <li>
-          <Link href="/about" className="block py-2 pl-3 pr-4 text-white rounded md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
-        </li>
-        <li>
-          <Link href="/announcements" className="block py-0 pl-3 pr-4 text-white rounded md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Announcements</Link>
-        </li>
-        <li>
-          <a href="#" className="block py-2 pl-3 pr-4 text-white rounded md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
+        
       </ul>
     </div>
   </div>
---
+++ NextJs/next_fire/app/signin/loading.jsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>Loading</div>
+  )
+}
+
+export default loading
