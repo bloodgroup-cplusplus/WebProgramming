747648a1aad847aa48513baa5deb90d9d5a74a93
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-13 15:32:58 +0530

    adding components

--- NextJs/next_fire/app/components/Courses.jsx
+++ NextJs/next_fire/app/components/Courses.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import prof from "../Images/prof.jpeg"
-const Courses = () => {
+const Courses = (props) => {
   return (
     <section className="bg-white dark:bg-gray-900">
+        <p></p>
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
           <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Humanities</h2>
--- NextJs/next_fire/app/courses/page.jsx
+++ NextJs/next_fire/app/courses/page.jsx
@@ -1,22 +1,25 @@
 'use client'
 import React from 'react'
 import Courses from '../components/Courses'
-import { useRouter } from 'next/router'
+import { usePathname,useSearchParams } from 'next/navigation';
+function useNavigationEvent()
+{
+  const pathname= usePathname()
+  const searchParams = useSearchParams()
+  const value=searchParams.toString()
+  return value.substring(value.indexOf("=")+1)
 
-export async function getServerSideProps({context}){
-  const param = context.paramName;
-  console.log(param)
 }
 
-const Page = async () => {
+const Page =  () => {
+  const college_name = useNavigationEvent()
 
 
   return (
             <>
           <h2 className="mb-4 text-4xl  text-center tracking-tight font-extrabold text-gray-900 dark:text-white">Under Graduate Programs</h2>
           <p className="text-gray-500 sm:text-xl text-center dark:text-gray-400">SGC Namchi offers 19 different Under Graduate Programs </p>
-          <p>{keyword}</p>
-          <Courses/>
+          <Courses college={college_name} />
           </>
 
 
