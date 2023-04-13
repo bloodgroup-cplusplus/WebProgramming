633b11f1eda91ee8001bb6b97a6581721704c160
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-13 20:32:13 +0530

    under construction

--- ReactProjectBasedLearning/FlowBite/my-project/src/Components/Navbar.jsx
+++ ReactProjectBasedLearning/FlowBite/my-project/src/Components/Navbar.jsx
@@ -40,7 +40,7 @@ import {useLogout} from "../Hooks/useLogout"
         </li>
 
         <li>
-          <a href="https://rzp.io/l/AnXnEvBNS" target="_blank" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-grey-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Donate</a>
+          <a href="https://rzp.io/l/AnXnEvBNS" target="_blank" className="block py-2 pl-3 pr-4 text-white rounded  md:hover:bg-transparent md:border-0 md:hover:text-grey-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Donate</a>
         </li>
       </ul>
 
