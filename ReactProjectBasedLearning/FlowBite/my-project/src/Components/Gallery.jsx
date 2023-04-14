3d2ba22674d2b0f462258cc01c8de6703faf7a4c
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-14 14:54:43 +0530

    added bunch of stuff

--- ReactProjectBasedLearning/FlowBite/my-project/src/Components/Gallery.jsx
+++ ReactProjectBasedLearning/FlowBite/my-project/src/Components/Gallery.jsx
@@ -15,85 +15,85 @@ import blood_again from "./Images/blood_again.webp"
 function Gallery() {
   return (
     <div>
-        <section class="overflow-hidden text-gray-700 ">
-        <h2 class="text-center mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Gallery</h2>
-  <div class="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-    <div class="flex flex-wrap -m-1 md:-m-2">
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+        <section className="overflow-hidden text-gray-700 ">
+        <h2 className="text-center mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Gallery</h2>
+  <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
+    <div className="flex flex-wrap -m-1 md:-m-2">
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={history}/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={new_members}/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={mala_press}/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={new_members_press}/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={sanitization_drive}/>
         </div>
       </div>
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={blood_donation}/>
         </div>
       </div>
 
 
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={latest_photo}/>
         </div>
       </div>
 
 
 
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={president_press}/>
         </div>
       </div>
 
 
-     <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+     <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={zoom}/>
         </div>
       </div>
 
 
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={with_banner}/>
         </div>
       </div>
 
 
 
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={with_cm}/>
         </div>
       </div>
@@ -101,67 +101,12 @@ function Gallery() {
 
 
 
-      <div class="flex flex-wrap w-1/3">
-        <div class="w-full p-1 md:p-2">
-          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+      <div className="flex flex-wrap w-1/3">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
             src={blood_again}/>
         </div>
       </div>
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     </div>
   </div>
 </section>
