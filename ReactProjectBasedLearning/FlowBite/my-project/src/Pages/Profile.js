d37e816accd4d0d4a6172567ac697fbb95444ac2
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-14 14:00:36 +0530

    changed class in profile to classname

--- ReactProjectBasedLearning/FlowBite/my-project/src/Pages/Profile.js
+++ ReactProjectBasedLearning/FlowBite/my-project/src/Pages/Profile.js
@@ -14,20 +14,20 @@ const Profile = () => {
     <>
 
 
-{result &&<div class="w-full max-w-sm bg-white border justify-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <div class="flex justify-center px-4 pt-4">
+{result &&<div className="w-full max-w-sm bg-white border justify-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex justify-center px-4 pt-4">
        
        
     </div>
-    <div class="flex justify-center md:m-auto flex-col items-center pb-10">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={sta_logo} alt="sta_logo"/>
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Sikkim Teachers Association</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">TEACHERS NAME: {result['displayName']} </span> 
-        <span class="text-sm text-gray-500 dark:text-gray-400">SCHOOL:{result['school']} </span>
-        <span class="text-sm text-gray-500 dark:text-gray-400">DESIGNATION: {result['designation']} </span>
-        <span class="text-sm text-gray-500 dark:text-gray-400">STA ID: {result['sta_id']} </span>
-        <span class="text-sm text-gray-500 dark:text-gray-400">VALID UPTO: 31 DECEMBER 2023 </span>
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={seal} alt="sta_seal"/>
+    <div className="flex justify-center md:m-auto flex-col items-center pb-10">
+        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={sta_logo} alt="sta_logo"/>
+        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Sikkim Teachers Association</h5>
+        <span className="text-sm text-gray-500 dark:text-gray-400">TEACHERS NAME: {result['displayName']} </span> 
+        <span className="text-sm text-gray-500 dark:text-gray-400">SCHOOL:{result['school']} </span>
+        <span className="text-sm text-gray-500 dark:text-gray-400">DESIGNATION: {result['designation']} </span>
+        <span className="text-sm text-gray-500 dark:text-gray-400">STA ID: {result['sta_id']} </span>
+        <span className="text-sm text-gray-500 dark:text-gray-400">VALID UPTO: 31 DECEMBER 2023 </span>
+        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={seal} alt="sta_seal"/>
       
 
 
