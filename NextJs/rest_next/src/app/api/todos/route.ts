a0b195cb0aa117d375b6821038a72a5e5c75a9ce
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-20 18:12:11 +0530

    POST route handler added

--- NextJs/rest_next/src/app/api/todos/route.ts
+++ NextJs/rest_next/src/app/api/todos/route.ts
@@ -34,3 +34,25 @@ export async function DELETE(request:Request)
     return NextResponse.json({"message":`Todo ${id} deleted`})
 
 }
+
+// post request 
+export async function POST(request:Request)
+{
+    const {userId,title} :Partial<Todo> = await request.json()
+
+    if(!userId || !title) return NextResponse.json({"message":"Missing required data"})
+
+    const res = await fetch(DATA_SOURCE_URL,{
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json', 
+            'API-KEY':API_KEY
+        }, 
+        body:JSON.stringify({
+            userId,title,complted:false
+        })
+    })
+    const newTodo: Todo = await res.json()
+
+    return NextResponse.json(newTodo)
+}
