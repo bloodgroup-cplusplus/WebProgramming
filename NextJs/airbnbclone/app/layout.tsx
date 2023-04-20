2c8f80cb06083b504a0cd874a3b6c37aa8a93cd4
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-20 18:02:58 +0530

    delete route created

--- NextJs/airbnbclone/app/layout.tsx
+++ NextJs/airbnbclone/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import {Nunito} from "next/font/google"
+import Navbar from './components/navbar/Navbar'
 
 
 
@@ -19,7 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>{children}
+      <Navbar/>
+      </body>
     </html>
   )
 }
--- NextJs/rest_next/src/app/api/todos/route.ts
+++ NextJs/rest_next/src/app/api/todos/route.ts
@@ -2,12 +2,35 @@ import { NextResponse } from "next/server";
 
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
+// define api key 
 
+const API_KEY:string = process.env.DATA_API_KEY as string
 
+// get route
 
 export async function GET(){
 
     const res = await fetch(DATA_SOURCE_URL)
     const todos: Todo[] =  await res.json()
     return NextResponse.json(todos)
+}
+
+
+// to delte the route 
+
+export async function DELETE(request:Request)
+{
+    const {id} : Partial<Todo> = await request.json() // there is an error in next js 13.3.0 
+    if(!id) return NextResponse.json({"message":"Todo id required"})
+
+    await fetch(`${DATA_SOURCE_URL}/${id}`,{
+        method:'DELETE',
+        headers:{
+            'Content-Type':'application/json',
+            'API-Key':API_KEY
+        }
+    })
+
+    return NextResponse.json({"message":`Todo ${id} deleted`})
+
 }
