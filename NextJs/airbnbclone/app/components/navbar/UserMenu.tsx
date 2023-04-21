da749a594e73f6bde319578b55ab11f44e8e13f0
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-21 09:48:40 +0530

    creating menu items

--- NextJs/airbnbclone/app/components/navbar/UserMenu.tsx
+++ NextJs/airbnbclone/app/components/navbar/UserMenu.tsx
@@ -1,8 +1,16 @@
 'use client';
 import {AiOutlineMenu} from "react-icons/ai"
 import Avatar from "../Avatar"
+import { useState } from "react";
+import { useCallback } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu =()=>{
+    const[isOpen,setIsOpen] = useState(false)
+    const toggleOpen = useCallback(()=>{
+        setIsOpen((value)=>!(value));
+
+    }, [])
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -10,7 +18,7 @@ const UserMenu =()=>{
                     Airbnb your home
 
                 </div>
-                <div onClick={()=>{}} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
+                <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
 
                 <AiOutlineMenu/>
                 <div className="hidden md:block">
@@ -18,6 +26,19 @@ const UserMenu =()=>{
                 </div>
                 </div>
             </div>
+            {isOpen  &&(
+                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                        <MenuItem/>
+                        </>
+                        
+                    </div>
+                    </div>
+
+        
+
+            )}
 
         </div>
     )
