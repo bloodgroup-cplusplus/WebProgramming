2cf06a289c948c59361e25ac6b45a8b7802a35de
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-04-21 17:06:56 +0530

    added bunch of stuff

--- NextJs/airbnbclone/app/components/modals/RegisterModal.tsx
+++ NextJs/airbnbclone/app/components/modals/RegisterModal.tsx
@@ -11,11 +11,60 @@ import {
 } from "react-hook-form"
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Modal from './Modal';
 
 
 const RegisterModal =() =>{
+    const registerModal = useRegisterModal();
+    const[isLoading,setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState:{
+            errors,
+        }
+    }= useForm<FieldValues>({
+        defaultValues:{
+            name:'',
+            email:'',
+            password:''
+        }
+    });
+
+    const onSubmit:SubmitHandler<FieldValues> =(data)=>{
+        setIsLoading(true);
+        // initiate axios post call to 
+        axios.post("/api/register",data).then(()=>{
+            registerModal.onClose()
+        }).catch((error)=>{
+            console.log(error);
+        })
+        .finally(()=>{
+            setIsLoading(false)
+        })
+    }
+
+
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <Heading/>
+            Hello Modal Body!
+
+        </div>
+    )
+
+
+
     return (
-        <div></div>
+        <Modal
+            disabled ={isLoading}
+            isOpen={registerModal.isOpen}
+            title="Register" 
+            actionLabel='Continue'
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+        />
     )
 
 }
--- NextJs/airbnbclone/app/components/navbar/UserMenu.tsx
+++ NextJs/airbnbclone/app/components/navbar/UserMenu.tsx
@@ -4,8 +4,10 @@ import Avatar from "../Avatar"
 import { useState } from "react";
 import { useCallback } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu =()=>{
+    const registerModal = useRegisterModal()
     const[isOpen,setIsOpen] = useState(false)
     const toggleOpen = useCallback(()=>{
         setIsOpen((value)=>!(value));
@@ -30,7 +32,15 @@ const UserMenu =()=>{
                 <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
                     <div className="flex flex-col cursor-pointer">
                         <>
-                        <MenuItem/>
+                        <MenuItem
+                            onClick={registerModal.onOpen}
+                            label="Sign up"
+                        />
+                        <MenuItem
+                            onClick={()=>{}}
+                            label = "Login"
+
+                            />
                         </>
                         
                     </div>
